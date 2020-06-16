2a3657b95e9889253afa0aecd7cb7996e8229aa3
WSchiller <62708205+WSchiller@users.noreply.github.com>
2020-06-15 20:17:40 -0400

    Update egypt.hpp

--- egypt.hpp
+++ egypt.hpp
@@ -1,9 +1,4 @@
-/*HPP*****************************************************************
-** FILENAME: egypt.hpp
-** Author: Wesley Schiller
-** Date: December 4, 2017
-** Description: Contains member function prototypes for Egypt.cpp
-*********************************************************************/
+// Contains member function prototypes for Egypt.cpp
 
 #ifndef EGYPT_HPP
 #define EGYPT_HPP
