45dcf079ea837ed4e89deb0cff54409e6ca808af
WSchiller <62708205+WSchiller@users.noreply.github.com>
2020-07-12 22:21:35 -0400

    Update medieval.cpp

--- medieval.cpp
+++ medieval.cpp
@@ -1,20 +1,6 @@
-/*CPP****************************************************************************************************
-** FILENAME: medieval.cpp
-** Author: Wesley Schiller
-** Date: December 4, 2017
-** Description: This file is for the Middle Age era.  It includes a brief introduction to the era,
-				displays a random trivia question to the user, then grades the user's answer against
-				the answer key.
-*********************************************************************************************************/
-
 #include "medieval.hpp"
 
-/* ----------------------------------------------------------Medieval--------
-| Function: Medieval
-| Purpose: constructor sets the string name, description, and relic
-| Parameters: none
-| Returns: none
-*----------------------------------------------------------------------------*/
+
 Medieval::Medieval()
 {
 	name = "Middle Ages";
@@ -24,12 +10,6 @@ Medieval::Medieval()
 	obtainedRelic = false;
 }
 
-/* ----------------------------------------------------displayEraInfo------
-| Function: displayEraInfo
-| Purpose: prints the era info - name and description
-| Parameters: none
-| Returns: none
-*------------------------------------------------------------------------*/
 void Medieval::displayEraInfo()
 {
 	cout << "Time Period: " << name << endl
@@ -37,12 +17,6 @@ void Medieval::displayEraInfo()
 		<< "Era Description: " << description << endl;
 }
 
-/* ---------------------------------------------------displayQuestion-----
-| Function: displayQuestion
-| Purpose: prints a randomly selected trivia question
-| Parameters: none
-| Returns: none
-*----------------------------------------------------------------------*/
 void Medieval::displayQuestion()
 {
 	int questionNumber = rand() % 4 + 1;
@@ -88,13 +62,6 @@ void Medieval::displayQuestion()
 	}
 }
 
-/* -------------------------------------------------------------------checkAnswer--------
-| Function: checkAnswer
-| Purpose: grades the answer pertaining to the trivia question
-| Parameters:
-|            string answer -- string of the user answer for the given question number
-| Returns: a boolean true if answer is correct, and false if incorrect
-*-------------------------------------------------------------------------------------*/
 bool Medieval::checkAnswer(string answer)
 {
 	string answerKey[]{ "b", "c", "b", "b", "d" };
@@ -116,12 +83,6 @@ bool Medieval::checkAnswer(string answer)
 	}
 }
 
-/* ---------------------------------------------------------------------~Medieval--------
-| Function: ~Medieval
-| Purpose : frees the dynamically allocated memory
-| Parameters : none
-| Returns : none
-*---------------------------------------------------------------------------------------*/
 Medieval::~Medieval()
 {
 	
