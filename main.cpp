da26d587b74b51f8ef705f7e4d22b93705ce137b
WSchiller <62708205+WSchiller@users.noreply.github.com>
2020-07-12 22:20:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,3 @@
-/*CPP****************************************************************************************************
-** FILENAME: main.cpp
-** Author: Wesley Schiller
-** Date: December 4, 2017
-** Description: The Time Machine game is a 1 player text-based game where the user is dealt the task of
-				collecting relics by traveling back in time to six different eras of history.  The user
-				has a 12 hour time limit to accomplish the task.  For each era the player will be asked
-				trivia question(s) pertaining to that time period.  The player must answer correctly to
-				obtain the relic. Each asked question incurrs a 1 hour time run-off.  Once the player
-				answers successfully, they can travel to another time period to collect one of the
-				remaining relics.  Collect all 6 relics before time expires and win!
-*********************************************************************************************************/
-
 #include "menu.hpp"
 #include "game.hpp"
 #include <iostream>
