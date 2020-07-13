104c61c6c7de01d632789b96874c466e816e78a3
WSchiller <62708205+WSchiller@users.noreply.github.com>
2020-07-12 22:08:35 -0400

    Update information.cpp

--- information.cpp
+++ information.cpp
@@ -1,20 +1,5 @@
-/*CPP****************************************************************************************************
-** FILENAME: information.cpp
-** Author: Wesley Schiller
-** Date: December 4, 2017
-** Description: This file is for the Information Age era.  It includes a brief introduction to the era,
-				displays a random trivia question to the user, then grades the user's answer against
-				the answer key.
-*********************************************************************************************************/
-
 #include "information.hpp"
 
-/* -----------------------------------------------------Information--------
-| Function: Information
-| Purpose: constructor sets the string name, description, and relic
-| Parameters: none
-| Returns: none
-*-------------------------------------------------------------------------*/
 
 Information::Information()
 {
@@ -25,12 +10,6 @@ Information::Information()
 	obtainedRelic = false;
 }
 
-/* ----------------------------------------------------displayEraInfo------
-| Function: displayEraInfo
-| Purpose: prints the era info - name and description
-| Parameters: none
-| Returns: none
-*-------------------------------------------------------------------------*/
 void Information::displayEraInfo()
 {
 	cout << "Time Period: " << name << endl
@@ -38,12 +17,7 @@ void Information::displayEraInfo()
 		<< "Era Description: " << description << endl;
 }
 
-/* ---------------------------------------------------displayQuestion-----
-| Function: displayQuestion
-| Purpose: prints a randomly selected trivia question
-| Parameters: none
-| Returns: none
-*-----------------------------------------------------------------------*/
+
 void Information::displayQuestion()
 {
 	int questionNumber = rand() % 4 + 1;
@@ -89,13 +63,7 @@ void Information::displayQuestion()
 	}
 }
 
-/* -------------------------------------------------------------------checkAnswer--------
-| Function: checkAnswer
-| Purpose: grades the answer pertaining to the trivia question
-| Parameters:
-|            string answer -- string of the user answer for the given question number
-| Returns: a boolean true if answer is correct, and false if incorrect
-*--------------------------------------------------------------------------------------*/
+
 bool Information::checkAnswer(string answer)
 {
 	string answerKey[]{ "d", "a", "c", "a", "d" };
@@ -105,7 +73,7 @@ bool Information::checkAnswer(string answer)
 		cout << "Correct answer! You have collected an artificial intelligence microchip!" << endl;
 		cout << "Historical Facts: From SIRI to self - driving cars, artificial intelligence(AI) is progressing rapidly.\n"
 			<< "While science fiction often portrays AI as robots with human - like characteristics,\n"
-			<< "AI can encompass anything from Google search algorithms to IBM’s Watson to autonomous weapons.\n";
+			<< "AI can encompass anything from Google search algorithms to IBMâ€™s Watson to autonomous weapons.\n";
 
 		return true;
 	}
@@ -116,12 +84,7 @@ bool Information::checkAnswer(string answer)
 	}
 }
 
-/* ------------------------------------------------------------------~Information--------
-| Function: ~Information
-| Purpose : frees the dynamically allocated memory
-| Parameters : none
-| Returns : none
-*---------------------------------------------------------------------------------------*/
+
 Information::~Information()
 {
 	
