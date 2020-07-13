7dc58d1b4ad4a94e2a39ad5b5f88c510010415ea
WSchiller <62708205+WSchiller@users.noreply.github.com>
2020-07-12 22:19:12 -0400

    Update jurassic.cpp

--- jurassic.cpp
+++ jurassic.cpp
@@ -1,20 +1,6 @@
-/*CPP****************************************************************************************************
-** FILENAME: jurassic.cpp
-** Author: Wesley Schiller
-** Date: December 4, 2017
-** Description: This file is for the Age of Dinosaur era.  It includes a brief introduction to the era,
-				displays a random trivia question to the user, then grades the user's answer against
-				the answer key.
-*********************************************************************************************************/
-
 #include "jurassic.hpp"
 
-/* ----------------------------------------------------------Jurassic--------
-| Function: Jurassic
-| Purpose: constructor sets the string name, description, and relic
-| Parameters: none
-| Returns: none
-*--------------------------------------------------------------------------*/
+
 Jurassic::Jurassic()
 {
 	name = "Age of the Dinosaur";
@@ -24,12 +10,6 @@ Jurassic::Jurassic()
 	obtainedRelic = false;
 }
 
-/* ----------------------------------------------------displayEraInfo------
-| Function: displayEraInfo
-| Purpose: prints the era info - name and description
-| Parameters: none
-| Returns: none
-*------------------------------------------------------------------------*/
 void Jurassic::displayEraInfo()
 {
 	cout << "Time Period: " << name << endl
@@ -37,12 +17,6 @@ void Jurassic::displayEraInfo()
 		<< "Era Description: " << description << endl;
 }
 
-/* ---------------------------------------------------displayQuestion-----
-| Function: displayQuestion
-| Purpose: prints a randomly selected trivia question
-| Parameters: none
-| Returns: none
-*-------------------------------------------------------------------------*/
 void Jurassic::displayQuestion()
 {
 	int questionNumber = rand() % 4 + 1;
@@ -88,13 +62,6 @@ void Jurassic::displayQuestion()
 	}
 }
 
-/* -------------------------------------------------------------------checkAnswer--------
-| Function: checkAnswer
-| Purpose: grades the answer pertaining to the trivia question
-| Parameters:
-|            string answer -- string of the user answer for the given question number
-| Returns: a boolean true if answer is correct, and false if incorrect
-*----------------------------------------------------------------------------------------*/
 bool Jurassic::checkAnswer(string answer)
 {
 	string answerKey[]{ "c", "a", "a", "d", "c" };
@@ -115,19 +82,7 @@ bool Jurassic::checkAnswer(string answer)
 	}
 }
 
-/* --------------------------------------------------------------------~Jurassic--------
-| Function: ~Jurassic
-| Purpose : frees the dynamically allocated memory
-| Parameters : none
-| Returns : none
-*---------------------------------------------------------------------------------------*/
 Jurassic::~Jurassic()
 {
 
 }
-
-
-
-
-
-
