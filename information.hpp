5b1040e34981e9e0b35767c4169d6fc0fb08ed02
WSchiller <62708205+WSchiller@users.noreply.github.com>
2020-07-12 22:17:49 -0400

    Update information.hpp

--- information.hpp
+++ information.hpp
@@ -1,13 +1,7 @@
-/*HPP********************************************************************
-** FILENAME: information.hpp
-** Author: Wesley Schiller
-** Date: December 4, 2017
-** Description: Contains member function prototypes for information.cpp
-*************************************************************************/
-
 #ifndef INFORMATION_HPP
 #define INFORMATION_HPP
 #include "space.hpp"
+
 
 class Information : public Space
 {
